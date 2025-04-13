be3cbbb8b7a1d69ac4270d3910e6e4538ea339cc
ashree2118 <ashree2118@gmail.com>
2025-04-13 23:51:17 +0530

    Update main.jsx

--- tictactoe/src/main.jsx
+++ tictactoe/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Square from './game.jsx';
+import Square from './ttt.jsx';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
     <Square />
+    
   </StrictMode>
 );
